c85d4836faa5eb474ee4c0f15000485b92f9d4ba
Apple enjoyer <maksim@MacBook-Pro-Maksim.local>
2022-10-15 14:19:41 +0300

    PR 15

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
 module.exports = (req, res, next) => {
+  const { NODE_ENV, JWT_SECRET } = process.env;
   const { authorization } = req.headers;
   const { cookies } = req;
 
@@ -14,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new AuthError('Необходима авторизация'));
     return;
